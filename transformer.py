43ba64ce7fe22fded53f74669328aff0beb449e9
Chris Schnaufer <schnaufer@email.arizona.edu>
2019-10-24 15:17:31 -0700

    Adding template files

---
+++ transformer.py
@@ -0,0 +1,33 @@
+"""Testing instance of transformer
+"""
+
+import argparse
+import transformer_class
+
+#pylint: disable=unused-argument
+def add_parameters(parser: argparse.ArgumentParser) -> None:
+    """Adds parameters
+    Arguments:
+        parser: instance of argparse
+    """
+
+def check_continue(transformer: transformer_class.Transformer, **kwargs) -> dict:
+    """Checks if conditions are right for continuing processing
+    Arguments:
+        transformer: instance of transformer class
+    Return:
+        Returns a dictionary containining the return code for continuing or not, and
+        an error message if there's an error
+    """
+    print("check_continue(): received arguments: %s" % str(kwargs))
+    return {'code': 0}
+
+def perform_process(transformer: transformer_class.Transformer, **kwargs) -> dict:
+    """Performs the processing of the data
+    Arguments:
+        transformer: instance of transformer class
+    Return:
+        Returns a dictionary with the results of processing
+    """
+    print("perform_process(): received arguments: %s" % str(kwargs))
+    return {'code': 0, 'message': "Everything is going swimmingly"}

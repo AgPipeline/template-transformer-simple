d1add7ca1892e53f10b8eae618027d4dff331b62
Chris Schnaufer <schnaufer@email.arizona.edu>
2019-10-25 16:40:15 -0700

    Template for Transformers - Initial Cut (#1) (#2)
    * Updating the README
    
    * Adding template files
    
    * Minor updates

---
+++ configuration.py
@@ -0,0 +1,26 @@
+"""Contains transformer configuration information
+"""
+
+# The version number of the transformer
+TRANSFORMER_VERSION = '1.0'
+
+# The transformer description
+TRANSFORMER_DESCRIPTION = ''
+
+# Short name of the transformer
+TRANSFORMER_NAME = ''
+
+# The sensor associated with the transformer
+TRANSFORMER_SENSOR = ''
+
+# The transformer type (eg: 'rgbmask', 'plotclipper')
+TRANSFORMER_TYPE = ''
+
+# The name of the author of the extractor
+AUTHOR_NAME = ''
+
+# The email of the author of the extractor
+AUTHOR_EMAIL = ''
+
+# Reposity URI of where the source code lives
+REPOSITORY = ''

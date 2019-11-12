9faf61593b6535ace3c5b00a6cb491f041198543
Sithyphus <jorgebarrios@email.arizona.edu>
2019-11-12 14:14:17 -0700

    Removed as of yet uneeded files

---
+++ test-files/test_transformer.py
@@ -0,0 +1,10 @@
+"""Testing files for transformer.py
+"""
+
+#Import transformer.py and imbedded modules
+import transformer
+import argparse
+import transformer_class
+
+#Setting up some testing classes
+test_transformer = transformer_class.Transformer()
